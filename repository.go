1778317544629e9290d29b67e814faf77b66524a
alexanderz <a.zhulin@12storeez.com>
2020-05-28 14:31:31 +0300

    add count docs method

--- repository.go
+++ repository.go
@@ -4,4 +4,5 @@ type Repository interface {
 	FindOne(condition map[string]interface{}) (*Feedback, error)
 	Update(filter, update map[string]interface{}) error
 	InsertOne(feedback *Feedback) error
+	CountFeedbacks(condition map[string]interface{}) (int, error)
 }
