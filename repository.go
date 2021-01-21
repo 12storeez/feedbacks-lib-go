993578a5b4f6096937aef21fa388fbb69cf55da3
Dmitry Vasyanin <dvasyanin0@gmail.com>
2021-01-21 16:31:04 +0300

    rf model

--- repository.go
+++ repository.go
@@ -1,19 +1,8 @@
 package feedbacks
 
-import "time"
-
 type Repository interface {
 	FindOne(condition map[string]interface{}) (*Feedback, error)
 	Update(filter, update map[string]interface{}) error
 	InsertOne(feedback *Feedback) error
 	CountFeedbacks(condition map[string]interface{}) (int, error)
-}
-
-// RepositoryPG...
-type RepositoryPG interface {
-	FindOneNoSentToSlack(afterBorderTime time.Time) (*Feedback, error)
-	Update(fb *Feedback) error
-	Insert(fb *Feedback) error
-	CountByArticle(article string) (int, error)
-	FindBy(id int) (*Feedback, error)
 }
