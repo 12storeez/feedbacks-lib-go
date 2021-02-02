c4f2b8be848791ca6f93f4bea13a27ed36e0983c
Dmitry Vasyanin <dvasyanin0@gmail.com>
2021-02-02 16:03:08 +0300

    add tags for pg

--- model.go
+++ model.go
@@ -6,38 +6,40 @@ import (
 )
 
 type Feedback struct {
-	Id                        primitive.ObjectID `bson:"_id" json:"-"`
-	Survey                    int                `json:"survey"`
-	Name                      string             `bson:",omitempty" json:"name"`
-	Email                     string             `bson:",omitempty" json:"email"`
-	Phone                     string             `bson:",omitempty" json:"phone"`
-	UserId                    string             `bson:"user_id,omitempty" json:"user_id"`
-	Contacts                  string             `bson:",omitempty" json:"contacts"`
-	Ts                        time.Time          `bson:",omitempty" json:"ts"`
-	Source                    string             `bson:",omitempty" json:"source"`
-	Date                      string             `bson:",omitempty" json:"date"`
-	Channel                   string             `bson:",omitempty" json:"channel"`
-	Nickname                  string             `bson:",omitempty" json:"nickname"`
-	ClientStatus              string             `bson:"client_status,omitempty" json:"client_status"`
-	Description               string             `bson:",omitempty" json:"description"`
-	FeedbackCategory          string             `bson:"feedback_category,omitempty" json:"feedback_category"`
-	ArticleCategory           string             `bson:"article_category,omitempty" json:"article_category"`
-	QualityCategory           string             `bson:"quality_category,omitempty" json:"quality_category"`
-	AcceptanceQualityCategory string             `bson:"acceptance_quality_category,omitempty" json:"acceptance_quality_category"`
-	Defect                    string             `bson:"defect,omitempty" json:"defect"`
-	Article                   string             `bson:",omitempty" json:"article"`
-	StoreDate                 string             `bson:"store_date,omitempty" json:"store_date"`
-	OrderId                   string             `bson:"order_id,omitempty" json:"order_id"`
-	Receipt                   string             `bson:",omitempty" json:"receipt"`
-	Store                     string             `bson:",omitempty" json:"store"`
-	User                      string             `bson:",omitempty" json:"user"`
-	Sent                      bool               `bson:"sent" json:"sent"`
-	MindboxSent               bool               `bson:"mindbox_sent" json:"mindbox_sent"`
-	ArticlesCount             map[string]int     `bson:"-" json:"-"`
-	Status                    string             `bson:",omitempty" json:"status"`
-	Conclusion                string             `bson:",omitempty" json:"conclusion"`
-	SourceType                string             `bson:"source_type,omitempty" json:"source_type"`
-	TransportCompany          string             `bson:"transport_company,omitempty" json:"transport_company"`
-	TransportCity             string             `bson:"transport_city,omitempty" json:"transport_city"`
-	Created                   time.Time          `bson:",omitempty" json:"created"`
+	ID                        int                `pg:"id,pk" bson:"-" json:"-"`
+	Id                        primitive.ObjectID `pg:"mongo_id" bson:"_id" json:"-"`
+	Survey                    int                `pg:"survey" json:"survey"`
+	Name                      string             `pg:"name" bson:",omitempty" json:"name"`
+	Email                     string             `pg:"email" bson:",omitempty" json:"email"`
+	Phone                     string             `pg:"phone" bson:",omitempty" json:"phone"`
+	UserId                    string             `pg:"user_id" bson:"user_id,omitempty" json:"user_id"`
+	Contacts                  string             `pg:"contacts" bson:",omitempty" json:"contacts"`
+	Ts                        time.Time          `pg:"ts" bson:",omitempty" json:"ts"`
+	Source                    string             `pg:"source" bson:",omitempty" json:"source"`
+	Date                      string             `pg:"date" bson:",omitempty" json:"date"`
+	Channel                   string             `pg:"channel" bson:",omitempty" json:"channel"`
+	Nickname                  string             `pg:"nickname" bson:",omitempty" json:"nickname"`
+	ClientStatus              string             `pg:"client_status" bson:"client_status,omitempty" json:"client_status"`
+	Description               string             `pg:"description" bson:",omitempty" json:"description"`
+	FeedbackCategory          string             `pg:"feedback_category" bson:"feedback_category,omitempty" json:"feedback_category"`
+	ArticleCategory           string             `pg:"article_category" bson:"article_category,omitempty" json:"article_category"`
+	QualityCategory           string             `pg:"quality_category" bson:"quality_category,omitempty" json:"quality_category"`
+	AcceptanceQualityCategory string             `pg:"acceptance_quality_category" bson:"acceptance_quality_category,omitempty" json:"acceptance_quality_category"`
+	Defect                    string             `pg:"defect" bson:"defect,omitempty" json:"defect"`
+	Article                   string             `pg:"article" bson:",omitempty" json:"article"`
+	StoreDate                 string             `pg:"store_date" bson:"store_date,omitempty" json:"store_date"`
+	OrderId                   string             `pg:"order_id" bson:"order_id,omitempty" json:"order_id"`
+	Receipt                   string             `pg:"receipt" bson:",omitempty" json:"receipt"`
+	Store                     string             `pg:"store" bson:",omitempty" json:"store"`
+	User                      string             `pg:"user" bson:",omitempty" json:"user"`
+	Sent                      bool               `pg:"sent" bson:"sent" json:"sent"`
+	MindboxSent               bool               `pg:"mindbox_sent" bson:"mindbox_sent" json:"mindbox_sent"`
+	ArticlesCount             map[string]int     `pg:"-" bson:"-" json:"-"`
+	Status                    string             `pg:"status" bson:",omitempty" json:"status"`
+	Conclusion                string             `pg:"conclusion" bson:",omitempty" json:"conclusion"`
+	SourceType                string             `pg:"source_type" bson:"source_type,omitempty" json:"source_type"`
+	TransportCompany          string             `pg:"transport_company" bson:"transport_company,omitempty" json:"transport_company"`
+	TransportCity             string             `pg:"transport_city" bson:"transport_city,omitempty" json:"transport_city"`
+	Created                   time.Time          `pg:"created" bson:",omitempty" json:"created"`
+	Updated                   time.Time          `pg:"updated" bson:"-" json:"-"`
 }
