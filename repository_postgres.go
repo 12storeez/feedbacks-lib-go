95882e915086ed7d5d27f7823f121e88dbc3c3b5
Dmitry Vasyanin <dvasyanin0@gmail.com>
2021-02-04 00:26:19 +0300

    add onconflict params

--- repository_postgres.go
+++ repository_postgres.go
@@ -63,7 +63,17 @@ func (p postgresRepository) Upsert(fb *Feedback) error {
 	if _, err := p.db.
 		Model(fb).
 		OnConflict("(id) DO UPDATE").
-		Set("updated = ?, sent = EXCLUDED.sent", time.Now()).
+		Set(`
+		mongo_id = EXCLUDED.mongo_id, survey = EXCLUDED.survey, name = EXCLUDED.name, email = EXCLUDED.email,
+		phone = EXCLUDED.phone, user_id = EXCLUDED.user_id, contacts = EXCLUDED.contacts, ts = EXCLUDED.ts,
+		source = EXCLUDED.source, date = EXCLUDED.date, channel = EXCLUDED.channel, nickname = EXCLUDED.nickname,
+		client_status = EXCLUDED.client_status, description = EXCLUDED.description, feedback_category = EXCLUDED.feedback_category, 
+		article_category = EXCLUDED.article_category, quality_category = EXCLUDED.quality_category, 
+		acceptance_quality_category = EXCLUDED.acceptance_quality_category, defect = EXCLUDED.defect,
+		article = EXCLUDED.article, store_date = EXCLUDED.store_date, order_id = EXCLUDED.order_id, receipt = EXCLUDED.receipt,
+		store = EXCLUDED.store, user = EXCLUDED.user, sent = EXCLUDED.sent, mindbox_sent = EXCLUDED.mindbox_sent,
+		status = EXCLUDED.status, conclusion = EXCLUDED.conclusion, source_type = EXCLUDED.source_type, transport_company = EXCLUDED.transport_company,
+		transport_city = EXCLUDED.transport_city, updated = ?`, time.Now()).
 		Insert(); err != nil {
 		return err
 	}
